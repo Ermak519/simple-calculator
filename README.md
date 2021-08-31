c3932706da78edd5a6e643096bd413ff602aca5b
Vyacheslav-Ubuntu18 <ermak519@gmail.com>
2021-08-31 17:01:44 +0300

    add README.md file

---
+++ README.md
@@ -0,0 +1,12 @@
+## Описание работы
+The function accepts arithmetic operations of two numbers as a string and returns a string with the result of their execution.
+
+- The calculator can perform operations of addition, subtraction, multiplication and division with two numbers: a + b, a - b, a * b, a / b. The data is transferred as a string.
+- The calculator can work with Arabic (1,2,3,4,5 ...) and Roman (I, II, III, IV, V ...) numbers.
+- The calculator accepts numbers from 1 to 10.
+- The calculator only works with integers.
+- The output is always a string with an integer.
+- The calculator works only with Arabic or Roman numerals at the same time, when you enter a string like "3 + II", the calculator throws an error.
+- Zero and negative numbers are missing in Roman system - the program returns an empty string.
+- The application throws an error when entering inappropriate numbers.
+- If you enter a string that does not match one of the above arithmetic operations, the application throws an error.
---
+++ calc.js
@@ -0,0 +1,119 @@
+function calculator(string){
+    function identifyInput(s) {
+        let arr = s.split(' ');
+        let arabic = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+        let rome = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X'];
+        if (arabic.includes(arr[0]) && arabic.includes(arr[2]) && arr.length <= 3) {
+            return 'arabic';
+        }
+        else if (rome.includes(arr[0]) && rome.includes(arr[2]) && arr.length <= 3) {
+            return 'rome';
+        }
+        else {
+            throw Error();
+        }
+    }
+    
+    function calculation(s) {
+        let arr = s.split(' ');
+        let operators = ["+", "-", "*", "/"];
+        let a = arr[0];
+        let b = arr[2];
+        let operator = arr[1];
+        let x = parseInt(a, 10);
+        let y = parseInt(b, 10);
+        if (operators.includes(operator)) {
+            switch (operator) {
+                case "+":
+                    return x + y;
+                case "-":
+                    return x - y;
+                case "*":
+                    return x * y;
+                case "/":
+                    return Math.floor(x / y);
+            }
+        }
+        throw Error();
+    }
+    
+    function toArab(s) {
+        let romeNumbers = {
+            "I": 1,
+            "II": 2,
+            "III": 3,
+            "IV": 4,
+            "V": 5,
+            "VI": 6,
+            "VII": 7,
+            "VIII": 8,
+            "IX": 9,
+            "X": 10
+        };
+        let numbers = s.split(' ');
+        let str = '';
+        for (let i = 0; i < numbers.length; i++) {
+            for (let el in romeNumbers) {
+                if (el == numbers[i]) {
+                    numbers[i] = romeNumbers[el];
+                }
+            }
+        }
+        str = numbers.join(' ');
+        return str;
+    }
+    
+    function toRome(num) {
+        let str = '';
+        let romanList = {
+            M: 1000,
+            CM: 900,
+            D: 500,
+            CD: 400,
+            C: 100,
+            XC: 90,
+            L: 50,
+            XL: 40,
+            X: 10,
+            IX: 9,
+            V: 5,
+            IV: 4,
+            I: 1
+        };
+        if (num > 0) {
+            for (let key in romanList) {
+                let a = Math.floor(num / romanList[key]);
+                if (a >= 0) {
+                    for (let i = 0; i < a; i++) {
+                        str += key;
+                    }
+                }
+                num = num % romanList[key];
+            }
+        }
+        else {
+            return '';
+        }
+        return str;
+    }
+    
+    function calc(s) {
+        let result = null;
+        let strRome = '';
+        let type = identifyInput(s); // rome or arab
+        if (type == 'arabic') {
+            result = calculation(s);
+        }
+        else if (type == 'rome') {
+            strRome = toArab(s);
+            result = calculation(strRome);
+            result = toRome(result);
+        }
+        return result.toString();
+        // console.log(result.toString())
+    }
+    
+    return calc(string)
+}
+
+module.exports = calculator; // Не трогайте эту строчку
